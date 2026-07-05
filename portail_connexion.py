1651871f3bdca89ab19ca4478d26576c5cf00278
abdoullahNoomwinde <yougabdoul.univ@gmail.com>
2026-07-05 07:57:22 +0000

    Ajout d'une  de vérification du mot de passe

--- portail_connexion.py
+++ portail_connexion.py
@@ -1,7 +1,25 @@
+def verifier_mot_de_passe():
+    entree_mot_de_passe = entry_mot_de_passe.get()
+    if entree_mot_de_passe == "qwerty":
+        label_resultat.config(text="Connexion réussie")
+        label_resultat.pack()
+    else:
+        label_resultat.config(text="Mot de passe incorrect")
+        label_resultat.pack()
 import tkinter as tk 
 fenetre = tk.Tk()
 fenetre.title("Portail de connexion")
 fenetre.geometry("400x300")
 label_mot_de_passe = tk.Label(fenetre, text="Mot de passe : ")
 label_mot_de_passe.pack()
+entry_mot_de_passe = tk.Entry(fenetre, show="*")
+entry_mot_de_passe.pack()
+label_resultat = tk.Label(fenetre, text="")
+label_resultat.pack()
+se_connecter = tk.Button(fenetre, text="Se connecter", command=verifier_mot_de_passe)
+se_connecter.pack()
 fenetre.mainloop()
+
+
+
+
