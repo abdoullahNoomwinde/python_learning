9c685c604120d1bb3789c2aff66a7a1cf4016701
abdoullahNoomwinde <yougabdoul.univ@gmail.com>
2026-07-07 22:42:18 +0000

    limite d'essai et désactivation du bouton pendant un certain temps

--- portail_connexion.py
+++ portail_connexion.py
@@ -1,14 +1,28 @@
+essai = 3
 def verifier_mot_de_passe():
+    global essai
     entree_mot_de_passe = entry_mot_de_passe.get()   
     if entree_mot_de_passe == "azerty":
         label_resultat.config(text="Connexion réussie", fg="green")
     else:
-        label_resultat.config(text=f"Mot de passe incorrect", fg="red")
+        essai -= 1
+        label_resultat.config(text=f"Mot de passe incorrect\nIl vous reste {essai} essai(s)", fg="red")
         entry_mot_de_passe.delete(0, tk.END)
         entry_mot_de_passe.focus_set()
+        if essai == 0:
+            label_resultat.config(text="Vous avez dépassé le nombre d'essais autorisés.\nVeuillez réessayer dans 5 secondes.", fg="red")
+            se_connecter.config(state="disabled")
+            fenetre.after(5000, reactiver_bouton)
 
-             
-import tkinter as tk 
+def reactiver_bouton():
+    global essai
+    essai = 3
+    se_connecter.config(state="normal")
+    entry_mot_de_passe.delete(0, tk.END)
+    entry_mot_de_passe.focus_set()
+    label_resultat.config(text="Vous pouvez essayer à nouveau.", fg="green")
+
+import tkinter as tk
 fenetre = tk.Tk()
 fenetre.title("Portail de connexion")
 fenetre.geometry("400x300")
@@ -27,7 +41,7 @@ fenetre.mainloop()
 #while essai < 0:
 #    print("Mot de passe incorrect /nIl vous reste", essai, "essai(s)")
 #if essai == 0:
-#    print("Vous avez dépassé le nombre d'essais autorisés.\nVeuillez réessayer dans 15 secondes.")
+#    print("Vous avez dépassé le nombre d'essais autorisés.\nVeuillez réessayer dans 5 secondes.")
 
   
 
